819c2ea6b70d699801d806afbbb9b70333d5a7b6
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-04 19:32:09 -0300

    area-quadrada
    001

---
+++ area-quadrada/bin/01-area-quadrado.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int areaDeQuadrado;
+  areaDeQuadrado = 3;
+
+  int calculoQuadrado = areaDeQuadrado * areaDeQuadrado;
+  String metros2 = "M²";
+
+  print("-------------CALCULO DE AREA--------------\n");
+  print("O calculo da Area de $areaDeQuadrado  é $calculoQuadrado$metros2");
+}
---
+++ area-quadrada/test/frenagem_test.dart
@@ -0,0 +1,8 @@
+import 'package:frenagem/frenagem.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
