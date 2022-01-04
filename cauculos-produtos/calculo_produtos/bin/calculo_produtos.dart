d0bc233f104f9628b36a3f0b02e43bf520dc70fd
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-04 19:59:37 -0300

    calculo-produto
    002

---
+++ cauculos-produtos/calculo_produtos/bin/calculo_produtos.dart
@@ -0,0 +1,14 @@
+import 'package:calculo_produtos/calculo_produtos.dart' as calculo_produtos;
+
+void main() {
+  int numero1 = 2;
+  int numero2 = 3;
+  int numero3 = 8;
+  int numero4 = 5;
+
+  int calculoproduto1 = numero1 * numero2;
+  int calculoproduto2 = numero3 * numero4;
+  int resultCalculo = calculoproduto1 + calculoproduto2;
+
+  print("O resultada do Calculo do Produto e $resultCalculo");
+}
---
+++ cauculos-produtos/calculo_produtos/test/calculo_produtos_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculo_produtos/calculo_produtos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
