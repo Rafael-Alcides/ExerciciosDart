5617530532153f8e4dfa631065804f7daa927023
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-05 20:29:34 -0300

    par-impar-zero
    11

---
+++ par-impar-zero/par_impar_zero/bin/par_impar_zero.dart
@@ -0,0 +1,14 @@
+import 'package:par_impar_zero/par_impar_zero.dart' as par_impar_zero;
+
+void main() {
+  int valor = 0;
+  if (valor == 0) {
+    print("Zero"); 
+  }
+
+  if ((valor % 2) == 0) {
+    print("Par");
+  } else {
+    print("Impar");
+  }
+}
---
+++ par-impar-zero/par_impar_zero/test/par_impar_zero_test.dart
@@ -0,0 +1,8 @@
+import 'package:par_impar_zero/par_impar_zero.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
