8bf827d46c75b3fe4ef1781f2a04de7bee3d36eb
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-06 12:22:15 -0300

    ano-bissesto
    016

---
+++ ano-bisesto/ano_bi/bin/ano_bi.dart
@@ -0,0 +1,14 @@
+import 'dart:typed_data';
+
+import 'package:ano_bi/ano_bi.dart' as ano_bi;
+import 'package:test/test.dart';
+
+void main() {
+  int ano = 1996;
+
+  if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+    print("Ano: $ano é bissexto!");
+  } else {
+    print("");
+  }
+}
---
+++ ano-bisesto/ano_bi/test/ano_bi_test.dart
@@ -0,0 +1,8 @@
+import 'package:ano_bi/ano_bi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
