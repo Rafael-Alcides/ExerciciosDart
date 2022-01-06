172f02e6ad9997e094baa9c36f13fc1f0efa4e84
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-06 19:46:44 -0300

    Contar string
    023

---
+++ contar-string/contar_string/bin/contar_string.dart
@@ -0,0 +1,8 @@
+import 'package:contar_string/contar_string.dart' as contar_string;
+
+void main() {
+  String palavra = "DartEAMelhorLinguagemDoMundo";
+
+  var palavracontada = palavra.length;
+  print(palavracontada);
+}
---
+++ contar-string/contar_string/test/contar_string_test.dart
@@ -0,0 +1,8 @@
+import 'package:contar_string/contar_string.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
