0ed0874784bfc5626e82eb899c28871bd15fc29e
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-07 20:23:25 -0300

    limitei casas decimais

--- saldo-devedor/saldo_devedor/bin/saldo_devedor.dart
+++ saldo-devedor/saldo_devedor/bin/saldo_devedor.dart
@@ -6,13 +6,13 @@ void main() {
   double valorDasPacelas = 566.78;
 
   double valorTotal = valorDasPacelas * totalDeParcelas;
-  String valorTotalString = valorTotal.toStringAsFixed(2);
+  
 
   double saldoDevedor = ((parcelasPagas * valorDasPacelas) - valorTotal);
   double saldo = saldoDevedor * -1;
-  String saldoDevedorString = saldo.toStringAsFixed(2);
+  
 
 
   print(
-      "Valor total da Consorsio: RS $valorTotalString\nSaldo devedor: RS $saldoDevedorString");
+      "Valor total da Consorsio: RS ${valorTotal.toStringAsFixed(2)}\nSaldo devedor: RS ${saldo.toStringAsFixed(2)}");
 }
