0ccc8bfa1371cba752ececf3e884b81174e34ac4
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-07 20:54:40 -0300

    alteracao

--- par-impar-zero/par_impar_zero/bin/par_impar_zero.dart
+++ par-impar-zero/par_impar_zero/bin/par_impar_zero.dart
@@ -2,13 +2,14 @@ import 'package:par_impar_zero/par_impar_zero.dart' as par_impar_zero;
 
 void main() {
   int valor = 0;
-  if (valor == 0) {
-    print("Zero"); 
+  
+
+  if (valor  == 0) {
+    print("Zero");
+  } else if(valor %  2 != 0) {
+    print("Impar");
+  }else {
+    print("Par"); 
   }
 
-  if ((valor % 2) == 0) {
-    print("Par");
-  } else {
-    print("Impar");
-  }
 }
