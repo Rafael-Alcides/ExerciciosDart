bf1b2f0e190b7d369550998fb5d4b8040806453e
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-07 20:01:11 -0300

    criada mais uma variavel

--- percentual desconto/perentual/bin/perentual.dart
+++ percentual desconto/perentual/bin/perentual.dart
@@ -1,9 +1,9 @@
 import 'package:perentual/perentual.dart' as perentual;
 
 void main() {
-   double valorInicial = 500;
+   double valorInicial = 500, valorDesconto = 10;
 
-  double desconto = valorInicial * (10 / 100);
+  double desconto = valorInicial * (valorDesconto / 100);
 
   double valorFinal = valorInicial - desconto;
 
