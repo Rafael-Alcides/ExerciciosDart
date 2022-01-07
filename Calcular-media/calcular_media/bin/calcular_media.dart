2be10b7d935e8aca0dfeea5fefdc3bafbe4542d3
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-07 20:32:02 -0300

    alteracao de var

--- Calcular-media/calcular_media/bin/calcular_media.dart
+++ Calcular-media/calcular_media/bin/calcular_media.dart
@@ -1,14 +1,13 @@
 import 'package:calcular_media/calcular_media.dart' as calcular_media;
 
 void main() {
-  double nota1 = 7;
-  double nota2 = 6.3;
-  double nota3 = 8;
+  num nota1 = 7;
+  num nota2 = 6.3;
+  num nota3 = 8;
 
-  double totalDeNotas = nota1 + nota2 + nota3;
-  double mediaTotal = totalDeNotas / 3;
+  num totalDeNotas = nota1 + nota2 + nota3;
+  num mediaTotal = totalDeNotas / 3;
 
-  final mediaFinal = mediaTotal.toStringAsFixed(1);
 
-  print("Média: $mediaFinal");
+  print("Média: ${mediaTotal.toStringAsFixed(1)}");
 }
