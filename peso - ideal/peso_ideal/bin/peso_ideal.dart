a57ac44c5497c0868ce42a4a47cf1ff670b6cbf7
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-07 21:45:38 -0300

    atualizacao de verificacao

--- peso - ideal/peso_ideal/bin/peso_ideal.dart
+++ peso - ideal/peso_ideal/bin/peso_ideal.dart
@@ -2,9 +2,22 @@ import 'package:peso_ideal/peso_ideal.dart' as peso_ideal;
 
 void main() {
   double altura = 1.85;
+  num calculo;
+  String sexo = "";
 
-  double calculoFeminino = 62.1 * altura - 44.7;
-  double calculoMasculino = 72.7 * altura - 58;
+  if (sexo == "f" || sexo == "F") {
+    calculo = 62.1 * altura - 44.7;
+  print({calculo.toStringAsFixed(3)});
+  } 
+  
+  else if (sexo == "m" || sexo == "M") {
+    calculo = 72.7 * altura - 58;
+    print({calculo.toStringAsFixed(3)});
+  } 
+  
+  else {
+    print("Escolha F ou f para FEMININO");
+    print("Escolha M ou m para MASCULINO");
+  }
 
-  print(calculoMasculino);
 }
