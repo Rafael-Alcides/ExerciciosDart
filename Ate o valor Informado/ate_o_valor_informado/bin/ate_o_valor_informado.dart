0564ecd9b95a0e41eb953ae360aa81635a2cdfcc
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-09 11:17:56 -0300

    resolucao

--- Ate o valor Informado/ate_o_valor_informado/bin/ate_o_valor_informado.dart
+++ Ate o valor Informado/ate_o_valor_informado/bin/ate_o_valor_informado.dart
@@ -3,8 +3,14 @@ import 'package:ate_o_valor_informado/ate_o_valor_informado.dart'
 
 void main() {
   int valor = 9;
+  String result = "";
 
   for (int i = 1; i <= valor; i++) {
-    print("$i,");
+    if (i < valor) {
+      result += "$i, ";
+    } else {
+      result += "$i.";
+    }
   }
+  print(result);
 }
