fcd1c46f222ea94cdf6394f116ffd157269cb989
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-09 12:44:24 -0300

    resolucao
    22

---
+++ Tamanho palavra/tamanho_palavra/bin/tamanho_palavra.dart
@@ -0,0 +1,10 @@
+import 'package:tamanho_palavra/tamanho_palavra.dart' as tamanho_palavra;
+
+void main() {
+  int tamanho;
+  String palavra = "Buenas";
+
+  tamanho = palavra.length;
+
+  print("A palavra \"$palavra\" tem $tamanho letras.");
+}
---
+++ Tamanho palavra/tamanho_palavra/test/tamanho_palavra_test.dart
@@ -0,0 +1,8 @@
+import 'package:tamanho_palavra/tamanho_palavra.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
